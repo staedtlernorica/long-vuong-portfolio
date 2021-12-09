85e21979873e2c1501beb1be84163a4056252d1c
Long Vuong <longvuong@live.com>
2021-12-09 17:39:22 -0500

    got nav to change color on scroll

---
+++ index.js
@@ -0,0 +1,37 @@
+// FOR POTENTIAL FUTURE USE WITH CHAING NAVBAR COLOR
+// https://usefulangle.com/post/113/javascript-detecting-element-visible-during-scroll
+
+
+
+// ILL GOTTEN GAIN FROM W3SCHOOL EXAMPLE
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementsByClassName("navAndDarkMode")[0].style.top = "0";
+    isElementInViewport();
+} else {
+    document.getElementsByClassName("navAndDarkMode")[0].style.top = "-60px";
+}
+  prevScrollpos = currentScrollPos;
+}
+
+
+
+// https://stackoverflow.com/a/7557433/6030118
+
+function isElementInViewport () {
+
+    const topBar = document.querySelector('.navAndDarkMode')
+    const resumeSection = document.querySelector('section.resume');
+    const contactSection = document.querySelector('section.contact');
+
+    var rect1 = resumeSection.getBoundingClientRect();
+    var rect2 = contactSection.getBoundingClientRect();
+    
+    // 
+    (rect1.top < 55 && rect1.bottom > 55) || (rect2.top < 55) ?
+    topBar.style.backgroundColor = 'rgba(232, 190, 163, 0.98)' :
+    topBar.style.backgroundColor = 'rgba(254, 250, 224, 0.98)';
+}
+
