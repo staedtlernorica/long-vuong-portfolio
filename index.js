e94ba9113a124ee556294de3fc0bb531ef31add9
Long Vuong <longvuong@live.com>
2021-12-26 15:14:48 -0500

    style update

--- index.js
+++ index.js
@@ -6,7 +6,7 @@
 // http://jsfiddle.net/n9uuv0hd/80/
 
 
-
+isElementInViewport();
 
 // ILL GOTTEN GAIN FROM W3SCHOOL EXAMPLE
 var prevScrollpos = window.pageYOffset;
@@ -15,7 +15,7 @@ window.onscroll = function () {
 
   const mobileMenuOpened = document.querySelector('nav ul').style.top === '26.8px'
 
-  // isElementInViewport();
+  isElementInViewport();
 
   if (prevScrollpos > currentScrollPos) {
     document.getElementsByClassName("navAndDarkMode")[0].style.top = "0";
@@ -33,23 +33,36 @@ window.onscroll = function () {
 
 function isElementInViewport() {
 
-  const resumeSection = document.querySelector('section.skills');
-  const contactSection = document.querySelector('section.contact');
+  const navDots = document.querySelectorAll('.pageIndicator i')
+  const navDot0 = navDots[0];
+  const navDot1 = navDots[1];
+  const navDot2 = navDots[2];
+
+  const rect1 = document.querySelector('header').getBoundingClientRect();
+  const rect2 = document.querySelector('#demos').getBoundingClientRect();
+  const rect3 = document.querySelector('#contact').getBoundingClientRect()
 
 
-  var rect1 = resumeSection.getBoundingClientRect();
-  var rect2 = contactSection.getBoundingClientRect();
+  // chain these together so they feed into one another instead of two of them having to handshake
 
-  // no more negative margins h2 so now just have 0 instead of 55
-  (rect1.top < 0 && rect1.bottom > 0) || (rect2.top < 0) ?
-    (
-     console.log(1)
-      ) :
-    (
-      console.log(2)
-      )
+  rect1.bottom > 320 ?
+    navDot0.className = 'fas fa-circle' :
+    navDot0.className = 'far fa-circle'
+
+  rect2.top < 380 && rect2.bottom > 480 ?
+    navDot1.className = 'fas fa-circle' :
+    navDot1.className = 'far fa-circle'
+
+  rect3.top < 520 ?
+    navDot2.className = 'fas fa-circle' :
+    navDot2.className = 'far fa-circle'
+
+
+
 }
-
+// const r2t = document.querySelector('#demos').getBoundingClientRect()['top'];
+// const r2b = document.querySelector('#demos').getBoundingClientRect()['bottom'];
+// const r2 = [r2t, r2b]
 
 
 document.querySelectorAll('.navAndDarkMode i').forEach((button) => {
@@ -66,18 +79,18 @@ function handleNavButton(button) {
   if (button.target.className.includes('moon')) {
 
     button.target.className === 'far fa-moon'
-      ? (button.target.className = 'fas fa-moon', 
-      button.target.style.transform = 'translate(5px, 5px)', 
-      alert('put on some sunglasses for now'))
-      : (button.target.className = 'far fa-moon', 
-      button.target.style.transform = 'translate(0, 0px)')
+      ? (button.target.className = 'fas fa-moon',
+        button.target.style.transform = 'translate(5px, 5px)',
+        alert('put on some sunglasses for now'))
+      : (button.target.className = 'far fa-moon',
+        button.target.style.transform = 'translate(0, 0px)')
 
   } else {
     button.target.style.transform === 'rotate(90deg)'
       ? (button.target.style.transform = 'rotate(0deg)',
         navUl.style.color = 'transparent',
         navUl.style.border = '3px solid transparent',
-        navUl.style.top = '-200px')
+        navUl.style.top = '-210px')
       : (button.target.style.transform = 'rotate(90deg)',
         navUl.style.top = '26.8px',
         navUl.style.border = '3px solid rgba(0,0,0,0.75)',
