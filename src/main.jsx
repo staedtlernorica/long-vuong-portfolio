539df382f23b42ed90acce3eef75e94f033e1437
staedtlernorica <longvuong@live.com>
2025-07-15 17:46:32 -0400

    added mantine

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { MantineProvider } from '@mantine/core';
 import App from './App';
-import './styles/main.css'; // if you have existing CSS
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <MantineProvider withGlobalStyles withNormalizeCSS>
+      <App />
+    </MantineProvider>
   </React.StrictMode>
 );
