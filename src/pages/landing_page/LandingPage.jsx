e85146c729ff088ca3200b2e05eb410d7c5b5e92
unknown <longvuong@live.com>
2025-11-06 23:00:27 -0500

    added fourth card

--- src/pages/landing_page/LandingPage.jsx
+++ src/pages/landing_page/LandingPage.jsx
@@ -1,44 +1,49 @@
-import { Container } from '@mui/material';
-import ArchiveIcon from '@mui/icons-material/Archive';
-import SystemUpdateAltRoundedIcon from '@mui/icons-material/SystemUpdateAltRounded';
-import ProjectCard from '../../components/ProjectCard';
+import { Container } from "@mui/material";
+import ArchiveIcon from "@mui/icons-material/Archive";
+import SystemUpdateAltRoundedIcon from "@mui/icons-material/SystemUpdateAltRounded";
+import ProjectCard from "../../components/ProjectCard";
+import PaymentsIcon from "@mui/icons-material/Payments";
 
 export default function Portfolio() {
   return (
     <Container>
-      <h1 className='my-15 text-center'>Featured Projects</h1>
+      <h1 className="my-15 text-center">Featured Projects</h1>
 
-      <div className='max-w-[1200px] grid grid-cols-[repeat(auto-fit,minmax(350px,1fr))] gap-8'>
+      <div className="max-w-[1200px] grid grid-cols-[repeat(auto-fit,minmax(350px,1fr))] gap-8">
         <ProjectCard
-          name='Meetup Archival'
-          description='Automated the archival of events and metadata for Meetup groups using Python, Selenium and PostgreSQL.'
+          name="Meetup Archival"
+          description="Automated the archival of events and metadata for Meetup groups using Python, Selenium and PostgreSQL."
           icon={<ArchiveIcon />}
-          tags={['Python', 'Selenium', 'Web Scraping', 'PostgreSQL']}
-          link='/projects/meetup-archival'
-          linkText='View Project'
+          tags={["Python", "Selenium", "Web Scraping", "PostgreSQL"]}
+          link="/projects/meetup-archival"
+          linkText="View Project"
         />
 
         <ProjectCard
-          name='GA4 Migration'
-          description='Migrated multiple clients from Universal Analytics to Google Analytics 4, ensuring data integrity and continuity of tracking with minimal disruption.'
+          name="Google Analytics 4 Tags Migration"
+          description="Integrated Google Analytics 4 tags into Google Tag Manager, ensuring continuity of tracking and privacy compliance with minimal disruption for enteprise clients."
           icon={<SystemUpdateAltRoundedIcon />}
-          tags={[
-            'Google Analytics 4',
-            'Google Tag Manager',
-            'Universal Analytics',
-            'Privacy Compliance',
-          ]}
-          link='/projects/ga4-migration'
-          linkText='View Case Study'
+          tags={["GA4", "GTM", "Privacy Compliance"]}
+          link="/projects/ga4-migration"
+          linkText="View Case Study"
         />
 
         <ProjectCard
-          name='Energy Analysis and Visualization'
-          description='Developed a comprehensive energy analysis and visualization tool using Python, Dash, and Plotly.'
-          icon={<SystemUpdateAltRoundedIcon />}
-          tags={['Python', 'Dash', 'Plotly', 'Data Visualization']}
-          link='/projects/energy-analysis'
-          linkText='View Case Study'
+          name="Price Comparison Visualizer"
+          description="Data visualization tool to help users compare electricity prices based on their daily usage data."
+          icon={<PaymentsIcon />}
+          tags={["Python", "Plotly", "Pandas", "Data Viz", "SQL", "ETL"]}
+          link="/projects/energy-analysis"
+          linkText="View Case Study"
+        />
+
+        <ProjectCard
+          name="Price Comparison Visualizer"
+          description="Data visualization tool to help users compare electricity prices based on their daily usage data."
+          icon={<PaymentsIcon />}
+          tags={["Python", "Plotly", "Pandas", "Data Viz", "SQL", "ETL"]}
+          link="/projects/energy-analysis"
+          linkText="View Case Study"
         />
       </div>
     </Container>
