48acb5dd214e56456538adac817cf51aec1d85b1
staedtlernorica <43657441+staedtlernorica@users.noreply.github.com>
2025-11-08 12:50:35 -0500

    Github deploy

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
-import { tanstackRouter } from "@tanstack/router-plugin/vite";
 
 export default defineConfig({
-  plugins: [react(), tailwindcss(), tanstackRouter()],
+  plugins: [react(), tailwindcss()],
 });
