77712b9995c5d1787c381746f4512592ead22dab
staedtlernorica <43657441+staedtlernorica@users.noreply.github.com>
2025-11-08 14:25:44 -0500

    Github deploy

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import tailwindcss from '@tailwindcss/vite';
+import tailwind from '@tailwindcss/vite';
 
-export default defineConfig({
-  plugins: [react(), tailwindcss()],
-});
+export default defineConfig(({ mode }) => ({
+  plugins: [react(), tailwind()],
+  base: mode === 'production' ? '/long-vuong-portfolio/' : '/',
+}));
